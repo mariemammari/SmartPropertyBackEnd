847be3356500e1681110ff7d7c1c60bffa0a4cdd
Mariem_Ammari <ammarim073@gmail.com>
2026-02-21 01:13:03 +0100

    Remove all tracked files to start clean

--- src/property/dto/update-property.dto.ts
+++
@@ -1,3 +0,0 @@
-import { CreatePropertyDto } from './create-property.dto';
-import { PartialType } from '@nestjs/mapped-types';
-export class UpdatePropertyDto extends PartialType(CreatePropertyDto) {}
