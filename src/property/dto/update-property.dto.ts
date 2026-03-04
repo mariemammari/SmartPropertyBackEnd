21f3f318fe9f64b0ab398e85948909f8d8c033ad
hana <hanafkiri81@gmail.com>
2026-03-04 01:51:49 +0100

    Add PropertyModule and update-property DTO

---
+++ src/property/dto/update-property.dto.ts
@@ -0,0 +1,3 @@
+import { CreatePropertyDto } from './create-property.dto';
+import { PartialType } from '@nestjs/mapped-types';
+export class UpdatePropertyDto extends PartialType(CreatePropertyDto) {}
