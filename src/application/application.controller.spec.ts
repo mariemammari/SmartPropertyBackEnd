9352e4373fd5550635dd2a71f63b791be9d3bcd4
Sofiene Zayati <sofiene.zayati@esprit.tn>
2026-04-29 13:27:42 +0100

    test unitaires

--- src/application/application.controller.spec.ts
+++ src/application/application.controller.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApplicationController } from './application.controller';
+import { ApplicationService } from './application.service';
 
 describe('ApplicationController', () => {
   let controller: ApplicationController;
 
+  const mockApplicationService = {
+    create: jest.fn(),
+    updateStatus: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ApplicationController],
+      providers: [
+        {
+          provide: ApplicationService,
+          useValue: mockApplicationService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ApplicationController>(ApplicationController);
